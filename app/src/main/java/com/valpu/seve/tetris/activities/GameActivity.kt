40e49222cb50ad770a77c945dd9ca3d11b3423f1
sevevaldez <cvvaldez00@gmail.com>
2018-09-24 00:08:42 -0500

    Add click-logic to buttons

--- app/src/main/java/com/valpu/seve/tetris/activities/GameActivity.kt
+++ app/src/main/java/com/valpu/seve/tetris/activities/GameActivity.kt
@@ -2,12 +2,35 @@ package com.valpu.seve.tetris.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.valpu.seve.tetris.R
+import com.valpu.seve.tetris.storage.AppPreferences
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
+
+    var tvHighScore: TextView? = null
+    var tvCurrentScore: TextView? = null
+    var appPreferences: AppPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        appPreferences = AppPreferences(this)
+        tvHighScore = tv_high_score
+        tvCurrentScore = tv_current_score
+
+        updateHighScore()
+        updateCurrentScore()
+
+    }
+
+    private fun updateHighScore() {
+        tvHighScore?.text = "${appPreferences?.getHighSocore()}"
+    }
+
+    private fun updateCurrentScore() {
+        tvCurrentScore?.text = "0"
     }
 }
